f597afca3443b3589473cd4a5c7c2687e35b8a6d
taxigps@sina.com <taxigps@sina.com@18895e1e-e9e5-11de-b362-3513cf04e977>
2011-03-30 01:10:32 +0000

    [script.module.keyboard.chinese] 升级至测试版0.2.0

--- addons/script.module.keyboard.chinese/lib/ChineseKeyboard.py
+++ addons/script.module.keyboard.chinese/lib/ChineseKeyboard.py
@@ -21,6 +21,18 @@ ACTION_PARENT_DIR     = 9
 ACTION_PREVIOUS_MENU  = 10
 ACTION_CONTEXT_MENU   = 117
 
+CTRL_ID_BACK = 8
+CTRL_ID_SPACE = 32
+CTRL_ID_RETN = 300
+CTRL_ID_LANG = 302
+CTRL_ID_CAPS = 303
+CTRL_ID_SYMB = 304
+CTRL_ID_LEFT = 305
+CTRL_ID_RIGHT = 306
+CTRL_ID_TEXT = 310
+CTRL_ID_CODE = 401
+CTRL_ID_HZLIST = 402
+
 a = ['a','ai','an','ang','ao','ba','bai','ban','bang','bao','be','bei','ben','beng','bi','bia','bian','biao','bie','bin','bing','bo','bu','ca','cai','can','cang','cao','ce','cen','ceng','ceok','ceom','ceon','ceor','cha','chai','chan','chang','chao','che','chen','cheng','chi','chong','chou','chu','chua','chuai','chuan','chuang','chui','chun','chuo','ci','cis','cong','cou','cu','cuan','cui','cun','cuo','da','dai','dan','dang','dao','de','dei','dem','den','deng','di','dia','dian','diao','die','dim','ding','diu','dong','dou','du','duan','dui','dul','dun','duo','e','en','eng','eo','eol','eom','eos','er','fa','fan','fang','fei','fen','feng','fo','fou','fu','ga','gad','gai','gan','gang','gao','ge','gei','gen','geng','gib','go','gong','gou','gu','gua','guai','guan','guang','gui','gun','guo','ha','hai','hal','han','hang','hao','he','hei','hen','heng','ho','hol','hong','hou','hu','hua','huai','huan','huang','hui','hun','huo','hwa','i','ji','jia','jian','jiang','jiao','jie','jin','jing','jiong','jiu','jou','ju','juan','jue','jun','ka','kai','kal','kan','kang','kao','ke','kei','ken','keng','ki','kong','kos','kou','ku','kua','kuai','kuan','kuang','kui','kun','kuo','kweok','kwi','la','lai','lan','lang','lao','le','lei','li','lia','lian','liang','liao','lie','lin','ling','liu','lo','long','lou','lu','luan','lue','lun','luo','lv','m','ma','mai','man','mang','mao','me','mei','men','meng','meo','mi','mian','miao','mie','min','ming','miu','mo','mou','mu','myeo','myeon','myeong','n','na','nai','nan','nang','nao','ne','nei','nem','nen','neng','neus','ng','ngag','ngai','ngam','ni','nian','niao','nie','nin','ning','niu','nong','nou','nu','nuan','nue','nun','nung','nuo','nv','nve','o','oes','ol','on','ou','pa','pai','pak','pan','pang','pao','pei','pen','peng','peol','phas','phdeng','phoi','phos','pi','pian','piao','pie','pin','ping','po','pou','ppun','pu','q','qi','qia','qian','qiang','qiao','qie','qin','qing','qiong','qiu','qu','quan','que','qun','ra','ram','ran','rang','rao','re','ren','ri','rong','rou','ru','rua','ruan','rui','run','ruo','sa','saeng','sai','sal','san','sang','sao','se','sed','sei','sen','seng','seo','seon','sha','shai','shan','shang','shao','she','shen','sheng','shi','shou','shu','shua','shuai','shuan','shuang','shui','shun','shuo','shw','si','so','sol','song','sou','su','suan','sui','sun','suo','ta','tae','tai','tan','tang','tao','teng','ti','tian','tiao','tie','ting','tol','ton','tong','tou','tu','tuan','tui','tun','tuo','uu','wa','wai','wan','wang','wei','wen','weng','wie','wo','wu','xi','xia','xian','xiang','xiao','xie','xin','xing','xiong','xiu','xu','xuan','xue','xun','ya','yan','yang','yao','ye','yi','yin','ying','yo','yong','you','yu','yuan','yue','yun','za','zad','zai','zan','zang','zao','ze','zei','zen','zeng','zha','zhai','zhan','zhang','zhao','zhe','zhen','zheng','zhi','zhong','zhou','zhu','zhua','zhuai','zhuan','zhuang','zhui','zhun','zhuo','zi','zo','zong','zou','zu','zuan','zui','zun','zuo']
 filename = os.path.join( __addonDir__, "lib", "pinyin.mb" )
 fd = open( filename, 'r')
@@ -29,15 +41,16 @@ fd.close()
 
 class InputWindow(xbmcgui.WindowXMLDialog):
 	def __init__( self, *args, **kwargs ):
-		self.action = None
+		self.totalpage = 1
 		self.nowpage = 0
+		self.words = ''
 		xbmcgui.WindowXMLDialog.__init__( self, *args, **kwargs )
 
 	def onInit(self):
 		self.status = 0
-		self.setKeyToChinese(0)
-		self.getControl(101).setLabel('')
-		self.getControl(2).setLabel('')
+		self.setKeyToChinese()
+		self.getControl(CTRL_ID_CODE).setLabel('')
+		self.getControl(CTRL_ID_TEXT).setLabel('')
 		self.confirmed = False
 		self.inputString = ''
 
@@ -45,56 +58,52 @@ class InputWindow(xbmcgui.WindowXMLDialog):
 		self.controlId = controlId
 
 	def onClick( self, controlID ):
-		if controlID == 332:#big
-			self.status = 3
-			self.setKeyToChinese(3)
-		elif controlID == 333:#num
-			self.status = 2
-			self.setKeyToChinese(2)
-		elif controlID == 335:#en
-			self.status = 1
-			self.setKeyToChinese(1)
-		elif controlID == 336:#ch
-			self.status = 0
-			self.setKeyToChinese(0)
-		elif controlID == 311:#back
-			if self.status==0 and len(self.getControl(101).getLabel())>0:
-				self.getControl(101).setLabel(self.getControl(101).getLabel()[0:-1])
-				self.getChineseWord(self.getControl(101).getLabel())
+		if controlID == CTRL_ID_CAPS:#big
+			self.getControl(CTRL_ID_SYMB).setSelected(False)
+			if self.getControl(CTRL_ID_CAPS).isSelected():
+				self.getControl(CTRL_ID_LANG).setSelected(False)
+			self.setKeyToChinese()
+		elif controlID == CTRL_ID_SYMB:#num
+			self.setKeyToChinese()
+		elif controlID == CTRL_ID_LANG:#en/ch
+			if self.getControl(CTRL_ID_LANG).isSelected():
+				self.getControl(CTRL_ID_CAPS).setSelected(False)
+			self.setKeyToChinese()
+		elif controlID == CTRL_ID_BACK:#back
+			if self.getControl(CTRL_ID_LANG).isSelected() and len(self.getControl(CTRL_ID_CODE).getLabel())>0:
+				self.getControl(CTRL_ID_CODE).setLabel(self.getControl(CTRL_ID_CODE).getLabel()[0:-1])
+				self.getChineseWord(self.getControl(CTRL_ID_CODE).getLabel())
 			else:
-				self.getControl(2).setLabel(self.getControl(2).getLabel().decode("utf-8")[0:-1])
-		elif controlID == 321:#enter
-			newText = self.getControl(2).getLabel()
+				self.getControl(CTRL_ID_TEXT).setLabel(self.getControl(CTRL_ID_TEXT).getLabel().decode("utf-8")[0:-1])
+		elif controlID == CTRL_ID_RETN:#enter
+			newText = self.getControl(CTRL_ID_TEXT).getLabel()
 			if not newText: return
 			self.inputString = newText
 			self.confirmed = True
 			self.close()
-			
-		elif controlID == 601:#left
-			if self.nowpage==0:
-				self.nowpage =3
-			else:
+		elif controlID == CTRL_ID_LEFT:#left
+			if self.nowpage>0:
 				self.nowpage -=1
 			self.changepages()
-		elif controlID == 602:#right
-			if self.nowpage==3:
-				self.nowpage =0
-			else:
+		elif controlID == CTRL_ID_RIGHT:#right
+			if self.nowpage<self.totalpage-1:
 				self.nowpage +=1
 			self.changepages()
-		elif controlID == 334:#write
-			self.getControl(2).setLabel(self.getControl(2).getLabel() + ' ')
+		elif controlID == CTRL_ID_SPACE:#space
+			self.getControl(CTRL_ID_TEXT).setLabel(self.getControl(CTRL_ID_TEXT).getLabel() + ' ')
 		else:
-			if self.status==0 and controlID!=329 and controlID!=330 and controlID!=331 and controlID<400:
-				s = self.getControl(101).getLabel() + self.getControl(controlID).getLabel()
-				self.getControl(101).setLabel(s)
-				self.getChineseWord(s)
-				#addfunction here
+			if self.getControl(CTRL_ID_LANG).isSelected() and not self.getControl(CTRL_ID_SYMB).isSelected():
+				if controlID>=65 and controlID<=90:
+					s = self.getControl(CTRL_ID_CODE).getLabel() + self.getControl(controlID).getLabel().lower()
+					self.getControl(CTRL_ID_CODE).setLabel(s)
+					self.getChineseWord(s)
+				elif controlID>=48 and controlID<=57:
+					i = self.nowpage*10*2+(controlID-48)*2
+					hanzi = unicode(self.words[i:(2+i)],'gbk').encode('utf-8')
+					self.getControl(CTRL_ID_TEXT).setLabel(self.getControl(CTRL_ID_TEXT).getLabel()+hanzi)
+					self.getControl(CTRL_ID_CODE).setLabel('')
 			else:
-				self.getControl(2).setLabel(self.getControl(2).getLabel().decode("utf-8")+self.getControl(controlID).getLabel())
-			
-			if controlID>400 and controlID<500:
-				self.setKeyToChinese(0)
+				self.getControl(CTRL_ID_TEXT).setLabel(self.getControl(CTRL_ID_TEXT).getLabel()+self.getControl(controlID).getLabel().encode('utf-8'))
 
 	def onAction(self,action):
 		#s1 = str(action.getId())
@@ -102,142 +111,91 @@ class InputWindow(xbmcgui.WindowXMLDialog):
 		#print "======="+s1+"========="+s2+"=========="
 		keycode = action.getButtonCode()
 		if keycode >= 61505 and keycode <= 61530:
-			if self.status==0:
+			if self.getControl(CTRL_ID_LANG).isSelected():
 				keychar = chr(keycode - 61505 + ord('a'))
-				s = self.getControl(101).getLabel() + keychar
-				self.getControl(101).setLabel(s)
+				s = self.getControl(CTRL_ID_CODE).getLabel() + keychar
+				self.getControl(CTRL_ID_CODE).setLabel(s)
 				self.getChineseWord(s)
 			else:
-				if self.status==3:
+				if self.getControl(CTRL_ID_CAPS).isSelected():
 					keychar = chr(keycode - 61505 + ord('A'))
 				else:
 					keychar = chr(keycode - 61505 + ord('a'))
-				self.getControl(2).setLabel(self.getControl(2).getLabel().decode("utf-8")+keychar)
+				self.getControl(CTRL_ID_TEXT).setLabel(self.getControl(CTRL_ID_TEXT).getLabel()+keychar)
 				
 		if action == ACTION_PARENT_DIR:
-			self.onClick( 311 )
+			self.onClick( CTRL_ID_BACK )
 		elif action == ACTION_PREVIOUS_MENU:
 			self.close()
 
 	def changepages (self):
-		tid = 151+self.nowpage
-		self.getControl(151).setVisible(False)
-		self.getControl(152).setVisible(False)
-		self.getControl(153).setVisible(False)
-		self.getControl(154).setVisible(False)
-		self.getControl(tid).setVisible(True)
-		
-		self.getControl(601).controlRight(self.getControl(18*self.nowpage+401))
-		self.getControl(602).controlLeft(self.getControl(18*self.nowpage+418))
-		for i in range(301, 311):
-			self.getControl(i).controlUp(self.getControl(18*self.nowpage+401))
-		#print "R-->"+str(18*self.nowpage+401)
+		self.getControl(CTRL_ID_HZLIST).setLabel('')
+		num=0
+		if self.nowpage == 0:
+			hzlist = ''
+		else:
+			hzlist = '<  '
+		for i in range(self.nowpage*10*2, len(self.words), 2):
+			hzlist = hzlist+str(num)+'.'+unicode(self.words[i:(2+i)],'gbk').encode('utf-8')+'  '
+			num+=1
+			if num > 9: break
+		if self.nowpage < self.totalpage-1:
+			hzlist = hzlist + '>'
+		self.getControl(CTRL_ID_HZLIST).setLabel(hzlist)
 
 	def getChineseWord (self,py):
 		self.nowpage = 0
-		self.changepages()
-		for i in range(401, 472):
-			self.getControl(i).setLabel('')
-
+		self.totalpage = 1
+		self.getControl(CTRL_ID_HZLIST).setLabel('')
 		if py in a:
-			words=b[a.index(py)]
-			keyid=401
-			for i in range(0, len(words), 2):
-				self.getControl(keyid).setLabel(unicode(words[i:(2+i)],'gbk').encode('utf-8'))
-				keyid+=1
-				if keyid > 472: break
+			self.words=b[a.index(py)]
+			self.totalpage = int(len(self.words)/2/10)+1
+			num=0
+			hzlist = ''
+			for i in range(0, len(self.words), 2):
+				hzlist = hzlist+str(num)+'.'+unicode(self.words[i:(2+i)],'gbk').encode('utf-8')+'  '
+				num+=1
+				if num > 9: break
+			if self.totalpage>1:
+				hzlist = hzlist + '>'
+			self.getControl(CTRL_ID_HZLIST).setLabel(hzlist)
 
-	def setKeyToChinese (self,t):
-		#print "========="+str(t)
-		if t==0 or t==1:		
-			self.getControl(101).setLabel('')
-			self.getControl(301).setLabel("q")
-			self.getControl(302).setLabel("w")
-			self.getControl(303).setLabel("e")
-			self.getControl(304).setLabel("r")
-			self.getControl(305).setLabel("t")
-			self.getControl(306).setLabel("y")
-			self.getControl(307).setLabel("u")
-			self.getControl(308).setLabel("i")
-			self.getControl(309).setLabel("o")
-			self.getControl(310).setLabel("p")
-			self.getControl(312).setLabel("a")
-			self.getControl(313).setLabel("s")
-			self.getControl(314).setLabel("d")
-			self.getControl(315).setLabel("f")
-			self.getControl(316).setLabel("g")
-			self.getControl(317).setLabel("h")
-			self.getControl(318).setLabel("j")
-			self.getControl(319).setLabel("k")
-			self.getControl(320).setLabel("l")
-			self.getControl(322).setLabel("z")
-			self.getControl(323).setLabel("x")
-			self.getControl(324).setLabel("c")
-			self.getControl(325).setLabel("v")
-			self.getControl(326).setLabel("b")
-			self.getControl(327).setLabel("n")
-			self.getControl(328).setLabel("m")
-		elif t==2:
-			self.getControl(301).setLabel("1")
-			self.getControl(302).setLabel("2")
-			self.getControl(303).setLabel("3")
-			self.getControl(304).setLabel("4")
-			self.getControl(305).setLabel("5")
-			self.getControl(306).setLabel("6")
-			self.getControl(307).setLabel("7")
-			self.getControl(308).setLabel("8")
-			self.getControl(309).setLabel("9")
-			self.getControl(310).setLabel("0")
-			self.getControl(312).setLabel("!")
-			self.getControl(313).setLabel("\\")
-			self.getControl(314).setLabel("#")
-			self.getControl(315).setLabel("$")
-			self.getControl(316).setLabel("%")
-			self.getControl(317).setLabel("^")
-			self.getControl(318).setLabel("&")
-			self.getControl(319).setLabel("*")
-			self.getControl(320).setLabel("-")
-			self.getControl(322).setLabel("(")
-			self.getControl(323).setLabel(")")
-			self.getControl(324).setLabel("=")
-			self.getControl(325).setLabel("+")
-			self.getControl(326).setLabel("/")
-			self.getControl(327).setLabel("[")
-			self.getControl(328).setLabel("]")
+	def setKeyToChinese (self):
+		self.getControl(CTRL_ID_CODE).setLabel('')
+		if self.getControl(CTRL_ID_SYMB).isSelected():
+			#if self.getControl(CTRL_ID_LANG).isSelected():
+			#	pass
+			#else:
+				i = 48
+				for c in ')!@#$%^&*(':
+					self.getControl(i).setLabel(c)
+					i+=1
+					if i > 57: break
+				i = 65
+				for c in '[]{}-_=+;:\'",.<>/?\\|`~':
+					self.getControl(i).setLabel(c)
+					i+=1
+					if i > 90: break
+				for j in range(i,90+1):
+					self.getControl(j).setLabel('')
 		else:
-			self.getControl(301).setLabel("Q")
-			self.getControl(302).setLabel("W")
-			self.getControl(303).setLabel("E")
-			self.getControl(304).setLabel("R")
-			self.getControl(305).setLabel("T")
-			self.getControl(306).setLabel("Y")
-			self.getControl(307).setLabel("U")
-			self.getControl(308).setLabel("I")
-			self.getControl(309).setLabel("O")
-			self.getControl(310).setLabel("P")
-			self.getControl(312).setLabel("A")
-			self.getControl(313).setLabel("S")
-			self.getControl(314).setLabel("D")
-			self.getControl(315).setLabel("F")
-			self.getControl(316).setLabel("G")
-			self.getControl(317).setLabel("H")
-			self.getControl(318).setLabel("J")
-			self.getControl(319).setLabel("K")
-			self.getControl(320).setLabel("L")
-			self.getControl(322).setLabel("Z")
-			self.getControl(323).setLabel("X")
-			self.getControl(324).setLabel("C")
-			self.getControl(325).setLabel("V")
-			self.getControl(326).setLabel("B")
-			self.getControl(327).setLabel("N")
-			self.getControl(328).setLabel("M")
-		
-		if t==0:
-			self.getControl(100).setVisible(True)
+			for i in range(48, 57+1):
+				keychar = chr(i - 48 + ord('0'))
+				self.getControl(i).setLabel(keychar)
+			if self.getControl(CTRL_ID_CAPS).isSelected():
+				for i in range(65, 90+1):
+					keychar = chr(i - 65 + ord('A'))
+					self.getControl(i).setLabel(keychar)
+			else:
+				for i in range(65, 90+1):
+					keychar = chr(i - 65 + ord('a'))
+					self.getControl(i).setLabel(keychar)
+		if self.getControl(CTRL_ID_LANG).isSelected():
+			self.getControl(400).setVisible(True)
 			self.nowpage = 0
 			self.changepages()
 		else:
-			self.getControl(100).setVisible(False)
+			self.getControl(400).setVisible(False)
 
 	def isConfirmed(self):
 		return self.confirmed
@@ -251,7 +209,7 @@ class Keyboard(object):
 		self.inputString = ''
 
 	def doModal (self):
-		self.win = InputWindow("script-input-windows.xml", __addonDir__, ADDON_SKIN )
+		self.win = InputWindow("DialogKeyboardChinese.xml", __addonDir__, ADDON_SKIN )
 		self.win.doModal()
 		self.confirmed = self.win.isConfirmed()
 		self.inputString = self.win.getText()
