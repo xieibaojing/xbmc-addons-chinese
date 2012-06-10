4ba440e0642a812286d8ed2a8d2a1d50e6887835
taxigps@sina.com <taxigps@sina.com@18895e1e-e9e5-11de-b362-3513cf04e977>
2012-06-10 08:11:13 +0000

    [plugin.video.qiyi] v2.0.3(dharma and eden) - 解决电影无法播放问题

--- addons/eden_src/plugin.video.qiyi/default.py
+++ addons/eden_src/plugin.video.qiyi/default.py
@@ -273,11 +273,16 @@ def seriesList(name,url,thumb):
 
 def PlayVideo(name,url,thumb):
     link = GetHttpData(url)
-    match1 = re.compile('pid : "(\d+)",//.*?ptype : "(\d+)",//.*?videoId : "([^"]+)",//', re.DOTALL).findall(link)
-    if len(match1) > 0:
-        pid = match1[0][0]
-        ptype = match1[0][1]
-        videoId = match1[0][2]
+    match1 = re.compile('<script type="text/javascript">\s*var info\s*=(.*?)</script>', re.DOTALL).findall(link)
+    if match1:
+        print match1
+        match1 = re.compile('"?pid"?:"(\d+)".*?"?ptype"?:"(\d+)".*?"?videoId"?:"([^"]+)"', re.DOTALL).findall(match1[0].replace(' ', ''))
+        if len(match1) > 0:
+            pid = match1[0][0]
+            ptype = match1[0][1]
+            videoId = match1[0][2]
+        else:
+            return
     else:
         return
     url = 'http://cache.video.qiyi.com/v/' + videoId + '/' + pid + '/' + ptype + '/'
