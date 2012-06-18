9bd7e4943d48e0c53bf619f2c96099d56602be2a
engcm@singnet.com.sg <engcm@singnet.com.sg@18895e1e-e9e5-11de-b362-3513cf04e977>
2012-06-18 11:43:06 +0000

    # Version 1.4.2 2012-06-18 (cmeng) # a. img url link given as full path #    => update def get_Thumb(icon):

--- addons/eden_src/plugin.video.yinyuetai/default.py
+++ addons/eden_src/plugin.video.yinyuetai/default.py
@@ -5,10 +5,9 @@ import xbmcaddon
 ######################################################
 # 音悦台MV
 ######################################################
-# Version 1.4.1 2012-02-19 (cmeng)
-# a. xbmcaddon.Addon() needs 1 parameter for Dharma
-# b. remove extra " in Artist name listing
-# c. addLink: name cannot contain "/" e.g. 12/02/25
+# Version 1.4.2 2012-06-18 (cmeng)
+# a. img url link given as full path
+#    => update def get_Thumb(icon):
 ######################################################
 
 __addonid__       = "plugin.video.yinyuetai"
@@ -43,7 +42,7 @@ def RecommendMV(url,name,mode):
 def ListRecommendMV(url,name,mode):
     link=get_content(url)
     if link == None: return
-    match0=re.compile('<div class="page-nav">(.+?)</div>').findall(link)   
+    match0=re.compile('<div class="page-nav" id="pageNav">(.+?)</div>').findall(link)   
     curpos=re.sub('推荐','',name)        
     try:
         curpage=re.compile('<span>(.+?)</span>').findall(match0[0])
@@ -54,10 +53,11 @@ def ListRecommendMV(url,name,mode):
     try:
         matchs=re.compile('<div class="recommend_mv_list"><ul>(.+?)</ul></div>').findall(link)
         matchli=re.compile('<li>(.+?)</li>').findall(matchs[0])
+
         total=len(matchli)
         j=0
+
         for item in matchli:
-            j+=1
             url1=re.compile('<a href="(.+?)" target="_blank"><img src="(.+?)" title="(.+?)" alt="(.+?)"/>').findall(item)
             p_artist=re.compile('--<a href="(.+?)" title="(.+?)" class="link_people"').findall(item)
             name = url1[0][2]
@@ -65,6 +65,7 @@ def ListRecommendMV(url,name,mode):
             pic = get_Thumb(url1[0][1])
             vurl=get_flv_url(get_realurl(url1[0][0]))             
             if vurl == None: continue
+            j+=1
             addLink(name,artist,vurl,pic,total,j)
     except:
         pass
@@ -108,7 +109,6 @@ def ListAllMV(url,name,mode):
         total=len(matchli)
         j=0
         for item in matchli:
-            j+=1
             img=re.compile('<img src="(.+?)"').findall(item)
             title=re.compile('<div class="title"><a href="(.+?)" title="(.+?)" target="_blank"').findall(item)
             artist=re.compile('<div class="artis">--<a href="(.+?)" title="(.+?)" class="link_people"').findall(item)
@@ -117,6 +117,7 @@ def ListAllMV(url,name,mode):
             pic = get_Thumb(img[0])
             vurl=get_flv_url(get_realurl(title[0][0])) 
             if vurl == None: continue
+            j+=1
             addLink(name,artist,vurl,pic,total,j)
     except:
         pass
@@ -167,13 +168,13 @@ def ShowPlayList(url,name,mode):
         if len(match1):
             j=0
             for items in match1:
-                j+=1
                 match=re.compile('<span name="(.+?)">(.+?)</span>').findall(items)
                 name = match[1][1]
                 artist = match[3][1]
                 pic = get_Thumb(match[4][1])
                 vurl=get_flv_url(get_realurl('/video/'+match[0][1])) 
                 if vurl == None: continue
+                j+=1
                 addLink(name,artist,vurl,pic,total,j)
 
 def get_artistpage(url,name,mode,handle):
@@ -272,7 +273,6 @@ def ShowFocusMV(url,name,mode):
         if total:
             j=0
             for item in matchli:
-                j+=1
                 img=re.compile('<img src="(.+?)"').findall(item)
                 title=re.compile('<div class="title"><a href="(.+?)" title="(.+?)" target="_blank" class="song">').findall(item)
                 p_artist=re.compile('--<a href="(.+?)" title="(.+?)" class="artist" target="_blank">').findall(item)
@@ -281,9 +281,11 @@ def ShowFocusMV(url,name,mode):
                 pic = get_Thumb(img[0])
                 vurl=get_flv_url(get_realurl(title[0][0]))
                 if vurl == None: continue
+                j+=1
                 addLink(name,artist,vurl,pic,total,j)
 
 def get_flv_url(url):
+    # http://www.flvcd.com/parse.php?flag=&format=&kw=http://3A%2F%2Fwww.yinyuetai.com%2Fvideo%2F389970&sbt=%BF%AA%CA%BCGO%21
     link=get_content('http://www.flvcd.com/parse.php?kw='+url)
     if link == None: return
     link=re.sub(" ","",link)
@@ -293,7 +295,7 @@ def get_flv_url(url):
     return
     
 def get_realurl(url):
-    return 'http://www.yinyuetai.com'+url
+    return 'http://www.yinyuetai.com' + url
 
 def get_content(url):
     req = urllib2.Request(url)
@@ -316,16 +318,20 @@ def get_content(url):
 def get_Thumb(icon):
     if len(icon) < 2:
         return __icon__
-    pic = __profile__ + icon.split('?')[0]
+
+    url = icon.split('?')[0]
+    len_http = len(url.split('/')[2]) + 8
+    pic = __profile__ + url[len_http:]
+
     if not os.path.isfile(pic):
         if not os.path.isdir(os.path.dirname(pic)):
             os.makedirs(os.path.dirname(pic))
         try:
-            pic=urllib.urlretrieve(get_realurl(icon), pic)[filename]
+            pic=urllib.urlretrieve(url, pic)[filename]
         except:
             pass
     return pic
-          
+
 def get_params():
     param=[]
     paramstring=sys.argv[2]
@@ -346,6 +352,7 @@ def get_params():
 def addLink(name,artist,url,pic,total,sn):
 #    u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&artist="+urllib.quote_plus(artist)
     name=re.sub('/','-',name) #cannot contains "/" e.g. 12/02/18
+    name=re.sub('&gt;','>', re.sub('&lt;','<',name))
     ok=True
     li=xbmcgui.ListItem(str(sn)+'. '+name+' 【'+artist+'】',iconImage=pic, thumbnailImage=pic)
     li.setInfo( type="Video", infoLabels={ "Title": name,"Artist": artist } )
@@ -389,21 +396,25 @@ ctl = {
             1    : ('RecommendMV(url,name,mode)','推荐MV','/lookVideo'),
             2    : ('ListRecommendMV(url,name,mode)','推荐MV'),
             4    : ('ListAllMV(url,name,mode)','全部MV'),
+         
             8    : ('SubTree(ctl,mode,(9,10,11,12))','热门评论悦单','/pl/playlist_hotComment'),
             9    : ('get_playlistpage(url,ctl[mode][1],mode)','24小时热门评论悦单','/pl/playlist_hotComment/today'),
             10   : ('get_playlistpage(url,ctl[mode][1],mode)','本周热门评论悦单','/pl/playlist_hotComment/week'),
             11   : ('get_playlistpage(url,ctl[mode][1],mode)','本月热门评论悦单','/pl/playlist_hotComment/month'),
             12   : ('get_playlistpage(url,ctl[mode][1],mode)','全部热门评论悦单','/pl/playlist_hotComment/all'),
+            
             13   : ('SubTree(ctl,mode,(14,15,16,17))','热门收藏悦单','/pl/playlist_hotFavorite'),
             14   : ('get_playlistpage(url,ctl[mode][1],mode)','24小时热门收藏悦单','/pl/playlist_hotFavorite/today'),
             15   : ('get_playlistpage(url,ctl[mode][1],mode)','本周热门收藏悦单','/pl/playlist_hotFavorite/week'),
             16   : ('get_playlistpage(url,ctl[mode][1],mode)','本月热门收藏悦单','/pl/playlist_hotFavorite/month'),
             17   : ('get_playlistpage(url,ctl[mode][1],mode)','全部热门收藏悦单','/pl/playlist_hotFavorite/all'),
+            
             18   : ('SubTree(ctl,mode,(19,20,21,22))','热门推荐悦单','/pl/playlist_hotRecommend'),
             19   : ('get_playlistpage(url,ctl[mode][1],mode)','24小时热门推荐悦单','/pl/playlist_hotRecommend/today'),
             20   : ('get_playlistpage(url,ctl[mode][1],mode)','本周热门推荐悦单','/pl/playlist_hotRecommend/week'),
             21   : ('get_playlistpage(url,ctl[mode][1],mode)','本月热门推荐悦单','/pl/playlist_hotRecommend/month'),
             22   : ('get_playlistpage(url,ctl[mode][1],mode)','全部热门推荐悦单','/pl/playlist_hotRecommend/all'),
+            
             23   : ('SubTree(ctl,mode,(24,25,26,27))','热门播放悦单','/pl/playlist_hotView'),
             24   : ('get_playlistpage(url,ctl[mode][1],mode)','24小时热门播放悦单','/pl/playlist_hotView/today'),
             25   : ('get_playlistpage(url,ctl[mode][1],mode)','本周热门播放悦单','/pl/playlist_hotView/week'),
@@ -414,14 +425,14 @@ ctl = {
             32   : ('get_playlistpage(url,ctl[mode][1],mode)','最新收藏悦单','/pl/playlist_newFavorite'),
             33   : ('get_playlistpage(url,ctl[mode][1],mode)','最新评论悦单','/pl/playlist_newComment'),
             34   : ('get_playlistpage(url,ctl[mode][1],mode)','最新创建悦单','/pl/playlist_newCreate'),
-            
+
             40   : ('get_artistpage(url,ctl[mode][1],mode,257)','全部歌手','/fanAll'),
-            41   : ('get_artistpage(url,ctl[mode][1],mode,257)','港台男歌手','/fanAll?area=HT&property=Boy'),
-            42   : ('get_artistpage(url,ctl[mode][1],mode,257)','港台女歌手','/fanAll?area=HT&property=Girl'),
-            43   : ('get_artistpage(url,ctl[mode][1],mode,257)','港台乐队/组合','/fanAll?area=HT&property=Combo'),
-            44   : ('get_artistpage(url,ctl[mode][1],mode,257)','内地男歌手','/fanAll?area=ML&property=Boy'),
-            45   : ('get_artistpage(url,ctl[mode][1],mode,257)','内地女歌手','/fanAll?area=ML&property=Girl'),
-            46   : ('get_artistpage(url,ctl[mode][1],mode,257)','内地乐队/组合','/fanAll?area=ML&property=Combo'),
+            41   : ('get_artistpage(url,ctl[mode][1],mode,257)','内地男歌手','/fanAll?area=ML&property=Boy'),
+            42   : ('get_artistpage(url,ctl[mode][1],mode,257)','内地女歌手','/fanAll?area=ML&property=Girl'),
+            43   : ('get_artistpage(url,ctl[mode][1],mode,257)','内地乐队/组合','/fanAll?area=ML&property=Combo'),
+            44   : ('get_artistpage(url,ctl[mode][1],mode,257)','港台男歌手','/fanAll?area=HT&property=Boy'),
+            45   : ('get_artistpage(url,ctl[mode][1],mode,257)','港台女歌手','/fanAll?area=HT&property=Girl'),
+            46   : ('get_artistpage(url,ctl[mode][1],mode,257)','港台乐队/组合','/fanAll?area=HT&property=Combo'),
             47   : ('get_artistpage(url,ctl[mode][1],mode,257)','欧美男歌手','/fanAll?area=US&property=Boy'),
             48   : ('get_artistpage(url,ctl[mode][1],mode,257)','欧美女歌手','/fanAll?area=US&property=Girl'),
             49   : ('get_artistpage(url,ctl[mode][1],mode,257)','欧美乐队/组合','/fanAll?area=US&property=Combo'),                   
@@ -431,21 +442,21 @@ ctl = {
             53   : ('get_artistpage(url,ctl[mode][1],mode,257)','日语男歌手','/fanAll?area=JP&property=Boy'),
             54   : ('get_artistpage(url,ctl[mode][1],mode,257)','日语女歌手','/fanAll?area=JP&property=Girl'),
             55   : ('get_artistpage(url,ctl[mode][1],mode,257)','日语乐队/组合','/fanAll?area=JP&property=Combo'),
-            
+
             61   : ('ListRecommendMV(url,name,mode)','全部推荐MV','/lookVideo-area/MV'),
-            62   : ('ListRecommendMV(url,name,mode)','港台推荐MV','/lookVideo-area/HT'),
-            63   : ('ListRecommendMV(url,name,mode)','内地推荐MV','/lookVideo-area/ML'),
+            62   : ('ListRecommendMV(url,name,mode)','内地推荐MV','/lookVideo-area/ML'),
+            63   : ('ListRecommendMV(url,name,mode)','港台推荐MV','/lookVideo-area/HT'),
             64   : ('ListRecommendMV(url,name,mode)','欧美推荐MV','/lookVideo-area/US'),
             65   : ('ListRecommendMV(url,name,mode)','韩语推荐MV','/lookVideo-area/KR'),           
-            66   : ('ListRecommendMV(url,name,mode)','日语推荐MV','/lookVideo-area/JP'),           
+            66   : ('ListRecommendMV(url,name,mode)','日语推荐MV','/lookVideo-area/JP'),      
             
             80   : ('ShowFocusMV(url,name,mode)','MV周榜','/index/MV'),
-            81   : ('ShowFocusMV(url,name,mode)','港台周榜','/index-ht'),
-            82   : ('ShowFocusMV(url,name,mode)','内地周榜','/index-ml'),
+            81   : ('ShowFocusMV(url,name,mode)','内地周榜','/index-ml'),
+            82   : ('ShowFocusMV(url,name,mode)','港台周榜','/index-ht'),
             83   : ('ShowFocusMV(url,name,mode)','欧美周榜','/index-us'),
             84   : ('ShowFocusMV(url,name,mode)','韩国周榜','/index-kr'),
-            85   : ('ShowFocusMV(url,name,mode)','日本周榜','/index-jp'),
-            
+            85   : ('ShowFocusMV(url,name,mode)','日本周榜','/index-jp'),            
+
             90   : ('SubTree(ctl,mode,(61,62,63,64,65,66))','推荐MV','/lookVideo'),
             91   : ('AllMV(url,name,mode)','全部MV','/lookAllVideo'),
             92   : ('SubTree(ctl,mode,(31,32,33,34))','最新悦单','/pl/playlist_new'),
@@ -453,7 +464,7 @@ ctl = {
             94   : ('get_playlistpage(url,ctl[mode][1],mode)','编辑推荐悦单','/pl/playlist_promo'),
             95   : ('get_playlistpage(url,ctl[mode][1],mode)','全部悦单','/pl/playlist_all'),
             96   : ('SubTree(ctl,mode,(40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55))','歌手','/fanAll'),
-            
+
             256  : ('ShowPlayList(url,name,mode)','显示悦单'),
             257  : ('ShowArtistMV(url,name,mode)','显示歌手MV'),
       }
