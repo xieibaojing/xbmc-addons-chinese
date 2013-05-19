a247fe34c6942569060ef338120453c8c94d4a3c
taxigps@sina.com <taxigps@sina.com@18895e1e-e9e5-11de-b362-3513cf04e977>
2013-05-19 00:24:15 +0000

    [plugin.audio.baidufm] v0.0.3 - 增加下一曲支持

--- addons/dharma_src/plugin.audio.baidufm/default.py
+++ addons/dharma_src/plugin.audio.baidufm/default.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 import xbmc, xbmcgui, xbmcaddon,  xbmcplugin
-import sys, urllib, urlparse
+import sys, urllib, urlparse, string
 
 if sys.version_info < (2, 7):
 	import simplejson
 else:
 	import json as simplejson
 
+__addon__ = xbmcaddon.Addon()
+__addonname__ = __addon__.getAddonInfo("name")
+
+def log(txt):
+	message = "%s: %s" % (__addonname__, unicode(txt).encode('utf-8'))
+	print(message)
+	#xbmc.log(msg = message, level = xbmc.LOGDEBUG)
+
 class BaiduFmPlayer(xbmc.Player):
 	def __init__(self):
 		xbmc.Player.__init__(self)
-		self.songIds = []
 		self.channelId = None
-		self.channelName = None
+		self.songInfos = []
+		self.songIds = []
+		self.playedSongs = []
+		self.playListSize = 1
 		self.playing = False
 	
 	def genPluginUrl(self, params):
@@ -38,71 +48,138 @@ class BaiduFmPlayer(xbmc.Player):
 		if action == None:
 			self.loadChannelList()
 		elif action == "playchannel":
-			self.channelId = self.getParam("channel_id");
-			self.channelName = self.getParam("channel_name");
-			self.loadSongList(self.channelId)
-			self.next()
-
-	def pushSongId(self, id):
-		self.songIds.append(id)
-
+			self.channelId = self.getParam("channel_id")
+			self.stop()
+			self.clearPlayList()
+			playlist = self.fillPlayList(self.playListSize)
+			self.play(playlist)
+			self.playing = True
+	
 	def loadChannelList(self):
 		html = urllib.urlopen("http://fm.baidu.com").read()
 		start = html.find("{", html.find("rawChannelList"))
-		end = html.find(";", start);
-		json = simplejson.loads(html[start:end].strip())
-		self.onChannelListLoaded(json["channel_list"])
-	
+		end = html.find(";", start)
+		json = html[start:end].strip()
+		#print json
+		data = simplejson.loads(json)
+		self.onChannelListLoaded(data["channel_list"])
+
 	def onChannelListLoaded(self, channelList):
+		totalItems = len(channelList)
 		for channel in channelList:
-			channelId = channel["channel_id"]
 			channelName = channel["channel_name"]
-			item = xbmcgui.ListItem(channel["channel_name"])
-			url = self.genPluginUrl({"action":"playchannel","channel_id":channelId,"channel_name":channelName})
-			xbmcplugin.addDirectoryItem(self.windowId, url, item)
-			#print url
-		xbmcplugin.setContent(self.windowId, 'music')
+			channelId = channel["channel_id"]
+			log("%s - %s" % (channelId, channelName))
+			item = xbmcgui.ListItem(channelName)
+			url = self.genPluginUrl({"action":"playchannel","channel_id":channelId})
+			xbmcplugin.addDirectoryItem(self.windowId, url, item, False, totalItems)
 		xbmcplugin.endOfDirectory(self.windowId)
+
+	def clearPlayList(self):
+		playlist = xbmc.PlayList(0)
+		playlist.clear()
+	
+	def fillPlayList(self, size):
+		playlist = xbmc.PlayList(0)
+		for i in range(size):
+			song = self.nextSong()
+			listItem = xbmcgui.ListItem(song["songName"], thumbnailImage=song["songPicRadio"])
+			listItem.setInfo(type="Music", infoLabels={"Title":song["songName"],"Artist":song["artistName"],"Album":song["albumName"]})
+			playlist.add(song["url"], listItem)
+		return playlist
+
+	def nextSong(self):
+		if(len(self.songInfos) <= 0):
+			while(len(self.songIds) < 10):
+				self.songIds += self.loadSongList(self.channelId)
+			ids = self.songIds[0:10]
+			self.songIds = self.songIds[10:]
+			self.songInfos += self.loadSongLinks(ids)
+		return self.songInfos.pop(0)
 
 	def loadSongList(self, channelId):
 		html = urllib.urlopen("http://fm.baidu.com/dev/api/?tn=playlist&format=json&id="+urllib.quote(channelId)).read()
 		json = simplejson.loads(html)
+		ids = []
 		for song in json["list"]:
-			self.pushSongId(song["id"])
+			ids.append(song["id"])
+		return ids
 
-	def playId(self, id):
-		id = str(id)
-		html = urllib.urlopen("http://music.baidu.com/data/music/fmlink?type=mp3&rate=320&songIds="+urllib.quote(id)).read()
-		json = simplejson.loads(html)
-		xcode = json["data"]["xcode"]
-		for song in json["data"]["songList"]:
-			if song["queryId"] == id:
-				listItem = xbmcgui.ListItem(song["songName"], thumbnailImage=song["songPicRadio"])
-				listItem.setInfo(type="music", infoLabels={"Title":song["songName"],"Artist":song["artistName"],"Album":song["albumName"],"Genre":self.channelName})
-				#print song
-				self.playing = True
-				self.play(song["songLink"] + "?xcode=" + xcode, listItem)
-				return
-		self.next()
+	def loadSongLinks(self, ids):
+		ids = string.join([str(i) for i in ids], ',')
+		html = urllib.urlopen("http://music.baidu.com/data/music/fmlink?type=mp3&rate=320&songIds="+urllib.quote(ids)).read()
+		data = simplejson.loads(html)
+		xcode = data["data"]["xcode"]
+		songs = data["data"]["songList"]
+		for song in songs:
+			song["url"] = song["songLink"] + "?xcode=" + xcode
+			log("%s - %s(%s)" % (song["songName"], song["artistName"], song["url"]))
+		return songs
 
-	def next(self):
-		if len(self.songIds) <= 0:
-			self.loadSongList(self.channelId)
-
-		id = self.songIds.pop(0)
-		self.playId(id)
+	def removePlayedItems(self):
+		playlist = xbmc.PlayList(0)
+		for song in self.playedSongs:
+			playlist.remove(song)
+			log("remove played:%s" % song)
+		self.playedSongs = []
+		return playlist
 	
+	def setCurrentPlaying(self):
+		try:
+			self.playedSongs.append(self.getPlayingFile())
+			log("%s:[%s]" % ("playedSongs", string.join(self.playedSongs, ",")))
+		except:
+			log("setCurrentPlaying Error")
+
 	def shouldClose(self):
 		return not self.playing
 
 	def onPlayBackEnded(self):
-		self.next()
+		log("onPlayBackEnded")
+		#self.stop()
+		#playlist = self.removePlayedItems()
+		#self.play(playlist)
+
+	def onPlayBackPaused(self):
+		log("onPlayBackPaused")
+
+	def onPlayBackResumed(self):
+		log("onPlayBackResumed")
+
+	def onPlayBackSeek(self):
+		log("onPlayBackSeek")
+
+	def onPlayBackSeekChapter(self):
+		log("onPlayBackSeekChapter")
+
+	def onPlayBackSpeedChanged(self):
+		log("onPlayBackSpeedChanged")
+
+	def onPlayBackStarted(self):
+		log("onPlayBackStarted")
+		self.playing = True
+		self.fillPlayList(1)
+		#self.removePlayedItems()
+		#self.setCurrentPlaying()
 
 	def onPlayBackStopped(self):
+		log("onPlayBackStopped")
+		self.clearPlayList()
 		self.playing = False
+
+	def onQueueNextItem(self):
+		log("onQueueNextItem")
+
+
+log("addon start")
 
 player = BaiduFmPlayer()
 player.parseArgv(sys.argv)
 
 while(not player.shouldClose()):
 	xbmc.sleep(1000)
+
+log("addon quit")
+
+
+
